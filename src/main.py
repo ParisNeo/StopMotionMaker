c1404dbc967efa1e5b82985f30a34916b2c5b1bc
Saifeddine <aloui.seifeddine@gmail.com>
2022-01-24 23:41:36 +0100

    Added timeline

--- src/main.py
+++ src/main.py
@@ -6,7 +6,7 @@
 # ===================
 
 import pygame
-from pygame_helpers import Action, Button, HorizontalLayout, ImageBox, VerticalLayout, WindowManager, MenuBar, Menu, Timer
+from pygame_helpers import Action, Button, HorizontalLayout, ImageBox, Slider, VerticalLayout, WindowManager, MenuBar, Menu, Timer
 from moviepy.editor import VideoFileClip
 
 # ===== Build pygame window and populate with widgets ===================
@@ -25,23 +25,41 @@ class MainWindow(WindowManager):
         self.layout_2 = VerticalLayout()
 
         self.main_video = ImageBox()
+        self.time_slider = Slider()
+        self.time_slider.value=0.5
+        self.time_slider.valueChanged_callback = self.slider_updated
+        self.time_slider.mouse_down_callback = self.slider_mouse_down
         self.test_ui1 = Button("Zone 1")
         self.test_ui3 = Button("Hello 3")
+
         self.layout_1.addWidget(self.test_ui1,0.2)
         self.layout_1.addWidget(self.layout_2,0.8)
 
         self.layout_2.addWidget(self.main_video,0.7)
-        self.layout_2.addWidget(self.test_ui3,0.3)
+        self.layout_2.addWidget(self.time_slider,0.05)
+        
+        self.layout_2.addWidget(self.test_ui3,0.25)
+
         self.addWidget(self.layout_1)
 
         self.motion_stuf = self.build_timer(self.do_stuf,1/24)
         self.motion_stuf.start()
-        self.i=0
-
+        self.t=0
+        self.paused = False
         self.clip = VideoFileClip(r'C:\Users\aloui\Videos\test\Terminator.mp4')
+
+    def slider_mouse_down(self):
+        self.paused = True
+        
+    def slider_updated(self, val):
+        self.t = self.clip.duration*val
+        self.paused = False
+
     def do_stuf(self):
-        self.main_video.setImage(self.clip.get_frame(self.i*1/24))
-        self.i += 1
+        if not self.paused:
+            self.main_video.setImage(self.clip.get_frame(self.t))
+            self.time_slider.setValue(self.t/self.clip.duration)
+            self.t += 1/24
 
     def fn_quit(self):
         self.Running=False
