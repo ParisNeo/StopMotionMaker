292d04dece07d56c0c654dc651deb62286a5d82e
Saifeddine <aloui.seifeddine@gmail.com>
2022-01-24 21:54:47 +0100

    Buildt main video zone

--- src/main.py
+++ src/main.py
@@ -6,7 +6,7 @@
 # ===================
 
 import pygame
-from pygame_helpers import Action, WindowManager, MenuBar, Menu, Timer
+from pygame_helpers import Action, Button, HorizontalLayout, ImageBox, VerticalLayout, WindowManager, MenuBar, Menu, Timer
 from moviepy.editor import VideoFileClip
 
 # ===== Build pygame window and populate with widgets ===================
@@ -21,11 +21,26 @@ class MainWindow(WindowManager):
         quit.clicked_event_handler = self.fn_quit
         self.edit = Menu(self.mn_bar,"Edit")
 
-        self.motion_stuf = self.build_timer(self.do_stuf,1)
+        self.layout_1 = HorizontalLayout()
+        self.layout_2 = VerticalLayout()
+
+        self.main_video = ImageBox()
+        self.test_ui1 = Button("Zone 1")
+        self.test_ui3 = Button("Hello 3")
+        self.layout_1.addWidget(self.test_ui1,0.2)
+        self.layout_1.addWidget(self.layout_2,0.8)
+
+        self.layout_2.addWidget(self.main_video,0.7)
+        self.layout_2.addWidget(self.test_ui3,0.3)
+        self.addWidget(self.layout_1)
+
+        self.motion_stuf = self.build_timer(self.do_stuf,1/24)
         self.motion_stuf.start()
         self.i=0
+
+        self.clip = VideoFileClip(r'C:\Users\aloui\Videos\test\Terminator.mp4')
     def do_stuf(self):
-        print(f"{self.i}")
+        self.main_video.setImage(self.clip.get_frame(self.i*1/24))
         self.i += 1
 
     def fn_quit(self):
@@ -33,7 +48,7 @@ class MainWindow(WindowManager):
     
 # =======================================================================
 
-#clip = VideoFileClip(r'C:\Users\aloui\Videos\test\Terminator.mp4')
+#
 #clip.preview()
 if __name__=="__main__":
     mw = MainWindow()
